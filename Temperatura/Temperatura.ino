58d249fb8cfb72266ef453fc132550370da1f6e1
Christian Steven Patiño Grisales <patino813@outlook.com>
2015-12-16 22:28:41 -0500

    Agregado el manejo de Modulo Rele

--- Temperatura/Temperatura.ino
+++ Temperatura/Temperatura.ino
@@ -5,15 +5,17 @@
 
 DHT dht(DHTPIN, DHTTYPE); //Definimos el nombre del sensor con "dht" y donde irá conectado junto con el modelo
 
+int cont = 0;
 const int sensor = 7; // Entrada del sensor
 const int led = 4; // Pin del LED
+const int rele2 = 13; // Pin del rele 2
 
 long miliVolts;
 long temperatura;
 
 void setup() {
   Serial.begin(9600); // Se inicia la comunicacion serial
-
+  pinMode(rele2, OUTPUT); 
 }
 
 void loop() {
@@ -30,6 +32,12 @@ void loop() {
   temperatura = miliVolts / 10; // Se calcula la temperatura
   
   float hi = dht.computeHeatIndex(t, h); //Calcular temperatura y humedad
+
+  
+  digitalWrite(rele2, HIGH); // Encendemos el LED
+  delay(5000);
+  digitalWrite(rele2, LOW); // Apagamos el LED
+  
   
   Serial.print("Temperatura: ");
   Serial.print(t);
@@ -40,5 +48,5 @@ void loop() {
   Serial.print(", HI: ");
   Serial.print(hi);
   Serial.println("%");
-  delay(2000);
+  delay(5000);
 }
